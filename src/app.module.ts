7e2d925fa53b9dcc417813b499843a1157cbbc34
Leonardo Minora <leonardo.minora@gmail.com>
2024-08-20 15:37:11 -0300

    adicionado endpoints para crud de tarefas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TarefasModule } from './tarefas/tarefas.module';
 
 @Module({
-  imports: [],
+  imports: [TarefasModule],
   controllers: [AppController],
   providers: [AppService],
 })
