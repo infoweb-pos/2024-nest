b842ec5ce3f4575ced5c72444bf01629ecee0971
Leonardo Minora <leonardo.minora@gmail.com>
2024-08-24 13:35:55 -0300

    módulo de persistência importado no módulo de tarefas

--- src/tarefas/tarefas.module.ts
+++ src/tarefas/tarefas.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TarefasService } from './tarefas.service';
 import { TarefasController } from './tarefas.controller';
+import { PersistenciaModule } from 'src/persistencia/persistencia.module';
 
 @Module({
   controllers: [TarefasController],
   providers: [TarefasService],
+  imports: [PersistenciaModule],
 })
 export class TarefasModule {}
